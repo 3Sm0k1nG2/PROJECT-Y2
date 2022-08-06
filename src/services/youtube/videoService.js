3f4b901d4ff6856736da1217a6842bcf4eed2537
3Sm0k1nG2 <3sm0k1ng2@Gmail.com>
2022-08-06 19:55:51 +0300

    Update videoService.js

--- src/services/youtube/videoService.js
+++ src/services/youtube/videoService.js
@@ -3,6 +3,5 @@ import * as request from '../requester';
 
 const url = `${urls.YOUTUBE}/videos`;
 
-export const get = (data) =>
-    request.get(url, data)
+export const get = (data) => request.get(url, data)
         .then(res => res.json());

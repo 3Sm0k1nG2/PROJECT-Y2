16f38f681eba97d2d86e2f71311ffdc77d454303
3Sm0k1nG2 <3sm0k1ng2@Gmail.com>
2022-08-06 19:56:23 +0300

    Update channelService.js

--- src/services/youtube/channelService.js
+++ src/services/youtube/channelService.js
@@ -4,7 +4,5 @@ import * as request from '../requester';
 
 const url = `${urls.YOUTUBE}/channels`;
 
-export const get = (data) => {
-    return request.get(url, data)
-        .then(res => {console.log(res); return res.json()})
-}
+export const get = (data) => request.get(url, data)
+        .then(res => res.json());

6e92202aec7668128220f0de0554a5abeab4536e
3Sm0k1nG2 <3sm0k1ng2@Gmail.com>
2022-08-09 19:31:19 +0300

    Testing purposes - REMOVE LATER

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
 import { useGoogleAuthContext } from '../../hooks/useGoogleAuthContext';
 
-import './Navbar.css';
+import styles from './Navbar.module.css';
+
+import { post } from '../../services/youtubeService';
 
 export const Navbar = () => {
     const { hasUser } = useGoogleAuthContext();
+
+    // REMOVE LATER - only for testing purposes {
+    const postNewCommentHandler = () => {
+        post.commentThreadsByVideoId();
+    }
+    // }
 
     return (
         <nav>
@@ -14,6 +22,11 @@ export const Navbar = () => {
             {hasUser &&
                 <Link to="/logout" >Logout</Link>
             }
+
+            {/* REMOVE LATER - only for testing purposes { */}
+            <Link to="/video/elkFVO1Alvk"> random video</Link>
+            <button onClick={postNewCommentHandler}> Post new comment </button>
+            {/* } */}
         </nav>
     );
 }

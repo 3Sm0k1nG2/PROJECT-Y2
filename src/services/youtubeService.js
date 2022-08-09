083bea2fe08dafae19eee669581ce82d04f6bcb5
3Sm0k1nG2 <3sm0k1ng2@Gmail.com>
2022-08-09 19:23:06 +0300

    Refactored youtubeService.js

--- src/services/youtubeService.js
+++ src/services/youtubeService.js
@@ -4,33 +4,38 @@ import { urls } from "../process";
 
 const baseUrl = urls.YOUTUBE;
 
+const reqYT = (resourcePath, data) => {
+    return request.get(`${baseUrl}/${resourcePath}`, data)
+        .then(res => res.json())
+        .catch(err => { throw err });
+}
+
 const get = {
-    videos: (data) => {
+    videos: reqYT.bind(null, 'videos'),
+    channels: reqYT.bind(null, 'channels'),
+    commentThreads: reqYT.bind(null, 'commentThreads'),
+    videoCategories: reqYT.bind(null, 'videoCategories'),
+    playlistItems: reqYT.bind(null, 'playlistItems')
+}
 
-        return request.get(`${baseUrl}/videos`, data)
+const post = {
+    commentThreadsByVideoId: (videoId = urls.TEST_VIDEO_ID, message = "testing...") => {
+        return request.post(`${baseUrl}/commentThreads`, {
+            "snippet": {
+                "videoId": videoId,
+                "topLevelComment": {
+                    "snippet": {
+                        "textOriginal": message
+                    }
+                }
+            }
+        })
             .then(res => res.json())
             .catch(err => { throw err });
-    },
-
-    channel: (data) => {
-        return request.get(`${baseUrl}/channels`, data)
-            .then(res => res.json())
-            .catch(err => { throw err });
-    },
-
-    commentThreads: (data) => {
-        return request.get(`${baseUrl}/commentThreads`, data)
-            .then(res => res.json())
-            .catch(err => { throw err });
-    },
-
-    videoCategories: (data) => {
-        return request.get(`${baseUrl}/videoCategories`, data)
-            .then(res => res.json())
-            .catch(err => { throw err });
-    },
+    }
 }
 
 export {
-    get
+    get,
+    post
 }
